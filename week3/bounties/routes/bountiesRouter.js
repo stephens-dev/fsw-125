29965388fd8bf758c0258b94044e8331dc8f916f
stephensWebDev <53384838+stephensWebDev@users.noreply.github.com>
2020-12-07 14:43:28 -0600

    bounties

--- week3/bounties/routes/bountiesRouter.js
+++ week3/bounties/routes/bountiesRouter.js
@@ -8,6 +8,25 @@ const bounties = [
    { firstName: "Darth", lastName: "Maul", Living: true, bounty: 40000, type: "Sith", _id: uuidv4() },
    { firstName: "Avix", lastName: "Mavr", Living: true, bounty: 8000, type: "Jedi", _id: uuidv4() }
 ]
+bountiesRouter.route("/:bountiesId")
+.get((req, res) => {
+    const bountiesId = req.params.bountiesId
+    const foundBounties = bounties.find(bounty => bounty._id === bountiesId)
+    res.send(foundBounties)
+})
+.put((req, res) => {
+    const bountyId = req.params.bountiesId
+    const updateBounty = req.body
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+    const updatedBounty = Object.assign(bounties[bountyIndex], updateBounty)
+    res.send("Updated")
+})
+.delete((req, res) => {
+    const bountyId = req.params.bountiesId
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+    bounties.splice(bountyIndex, 1)
+    res.send("Deleted")
+})
 
 
 bountiesRouter.route("/")
@@ -20,6 +39,7 @@ bountiesRouter.route("/")
         bounties.push(newBounty)
         res.send('Added ${newBounty} ')
     })
+   
 
 
 module.exports = bountiesRouter
